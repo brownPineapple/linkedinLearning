5badf351ccc538ad5416b66b39406edfed8ffa66
brownPineapple <30342446+brownPineapple@users.noreply.github.com>
2024-11-27 10:49:00 +0000

    Update factorial.py

--- pythonEssestialTraining/factorial.py
+++ pythonEssestialTraining/factorial.py
@@ -1,1 +1,19 @@
-#Code
+# Python code​​​​​​‌‌​​​​‌‌​‌​​​​​​‌​​​​‌‌​​ below
+
+def factorial(num):
+    if isinstance(num, int):
+        if num >= 1:
+            fact=1
+            for x in range(1,num+1):
+                fact = fact*x
+            return fact
+        elif num == 0:
+            return 1
+        else:
+            return None
+    else:
+        return None
+
+
+number = 1
+result = factorial(number)
