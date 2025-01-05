308e8f075a2831adf65ff2dd7fdf6b08822cb61d
brownPineapple <30342446+brownPineapple@users.noreply.github.com>
2025-01-05 10:10:56 +0000

    Update eggg_farmer_puzzle.py

--- collectionChallenges/eggg_farmer_puzzle.py
+++ collectionChallenges/eggg_farmer_puzzle.py
@@ -1,20 +1,24 @@
-# The Egg Farmer's Puzzle - www.101computing.net/the-egg-farmer-puzzle/
+# The Egg Farmer's Puzzle - www.101computing.net/the-egg-farmer-puzzle/ 
 
-#Complete your code here...
+print("EGG FARMER INTERFACE V1") 
+# Get the number of eggs picked up 
+egg_count = int(input("How many eggs did you pick up this morning?\nCount:")) 
 
-print("EGG FARMER INTERFACE V1")
+# Calculate the number of cartons of 12 and the remainder 
+cartons_of_12, remainder_eggs = divmod(egg_count, 12) 
 
-eggCount = int(input("How many eggs did you pick up this morning ?\nCount:"))
+# Determine the number of breakfast eggs 
+breakfast_eggs = remainder_eggs - 6 if remainder_eggs > 6 else remainder_eggs 
 
-box1, box2 = divmod(eggCount, 12)
-breakfastEggs = box2 - 6 if box2 > 6 else box2
+# Output the number of cartons of 12 
+print(f'You will need {cartons_of_12} cartons of 12') 
 
-print(f'You will need {box1} cartons of 12')
+# Output if an additional box of 6 is needed 
+if remainder_eggs > 6: print("You will need 1 box of 6") 
 
-if box2 > 6:
-   print("You will need 1 box of 6")
-   
-plural = 's' if breakfastEggs > 1 else '' 
-   
-if breakfastEggs > 0:
-   print(f"You will have {breakfastEggs} egg{plural} for breakfast") 
+# Determine the plural form for 'egg' 
+plural = 's' if breakfast_eggs != 1 else '' 
+
+# Output the number of breakfast eggs 
+if breakfast_eggs > 0:
+   print(f"You will have {breakfast_eggs} egg{plural} for breakfast")
